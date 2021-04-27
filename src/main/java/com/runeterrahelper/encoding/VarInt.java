6f5583960960d517116b13598cf5bac412859981
Edouard WILLISSECK <e-willisseck@efluid.fr>
2021-04-27 10:11:19 +0200

    TDD implementation of VarInt

--- src/main/java/com/runeterrahelper/encoding/VarInt.java
+++ src/main/java/com/runeterrahelper/encoding/VarInt.java
@@ -4,15 +4,17 @@ import java.util.List;
 
 public class VarInt {
 
+  private VarInt() {
+  }
+
   public static List<Integer> get(int integer) {
-    int count = 0;
-    while (integer > 127) {
-      count++;
-      integer -= 128;
-    }
-    if (count == 0) {
+    if (integer < 128)
       return List.of(integer);
+
+    int count = 1;
+    for (; integer > 255; integer -= 128) {
+      count++;
     }
-    return List.of(integer + 128, count);
+    return List.of(integer, count);
   }
 }
