806b13ddff27904297d7914cd00395851742b9b5
Édouard WILLISSECK <edouard.willisseck@gmail.com>
2021-05-13 16:47:03 +0200

    Implementing VarInt

--- src/main/java/com/runeterrahelper/encoding/VarInt.java
+++ src/main/java/com/runeterrahelper/encoding/VarInt.java
@@ -11,29 +11,35 @@ public class VarInt {
     private final List<Integer> varint = new ArrayList<>();
 
     public void add(int number) {
-        varint.addAll(VarInt.get(number));
+        List<Integer> res;
+        List<Integer> result = new ArrayList<>();
+        if (number < 128) {
+            result.add(number);
+            res = result;
+        } else {
+            int count = 1;
+            for (; number > 255; number -= 128) {
+                count++;
+            }
+            result.addAll(List.of(number, count));
+            res = result;
+        }
+
+        varint.addAll(res);
     }
     
     public int pop() {
-        return VarInt.pop(varint);
-    }
-    
-    public List<Integer> getValues() {
-        return varint;
-    }
-
-    public static int pop(List<Integer> bytes) {
         int result = 0;
         int shift = 0;
         int popped = 0;
 
-        for (int i = 0; i < bytes.size(); i++) {
+        for (int i = 0; i < varint.size(); i++) {
             popped++;
-            int current = bytes.get(i) & allButMSB;
+            int current = varint.get(i) & allButMSB;
             result |= current << shift;
 
-            if ((bytes.get(i) & justMSB) != justMSB) {
-                bytes.subList(0, popped).clear();
+            if ((varint.get(i) & justMSB) != justMSB) {
+                varint.subList(0, popped).clear();
                 return result;
             }
 
@@ -42,19 +48,8 @@ public class VarInt {
 
         throw new IllegalArgumentException("Byte array did not contain valid VarInts");
     }
-
-    public static List<Integer> get(int integer) {
-        List<Integer> result = new ArrayList<>();
-        if (integer < 128) {
-            result.add(integer);
-            return result;
-        }
-
-        int count = 1;
-        for (; integer > 255; integer -= 128) {
-            count++;
-        }
-        result.addAll(List.of(integer, count));
-        return result;
+    
+    public List<Integer> getValues() {
+        return varint;
     }
 }
