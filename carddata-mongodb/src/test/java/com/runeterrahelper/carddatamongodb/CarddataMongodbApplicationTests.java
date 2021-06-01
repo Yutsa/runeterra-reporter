8124c86fec0f39923b4b19a8fb7c6dab880856e7
Edouard WILLISSECK <e-willisseck@efluid.fr>
2021-06-01 08:17:58 +0200

    Changed project structure to have seperated modules.

---
+++ carddata-mongodb/src/test/java/com/runeterrahelper/carddatamongodb/CarddataMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.runeterrahelper.carddatamongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarddataMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
