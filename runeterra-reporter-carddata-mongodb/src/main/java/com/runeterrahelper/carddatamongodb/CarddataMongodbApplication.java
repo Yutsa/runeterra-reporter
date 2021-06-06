880029864eba392972169ee1c8fa634783a4b666
Édouard WILLISSECK <edouard.willisseck@gmail.com>
2021-06-06 11:52:30 +0200

    Cleaned up spring application

--- runeterra-reporter-carddata-mongodb/src/main/java/com/runeterrahelper/carddatamongodb/CarddataMongodbApplication.java
+++ runeterra-reporter-carddata-mongodb/src/main/java/com/runeterrahelper/carddatamongodb/CarddataMongodbApplication.java
@@ -1,19 +1,7 @@
 package com.runeterrahelper.carddatamongodb;
 
-import org.apache.commons.logging.*;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import com.runeterrahelper.cards.Card;
 
 @SpringBootApplication
 public class CarddataMongodbApplication {
-
-  private static final Log log = LogFactory.getLog(CarddataMongodbApplication.class);
-
-  public static void main(String[] args) {
-    SpringApplication.run(CarddataMongodbApplication.class, args);
-    new MongoCardRepository("runeterra").getCardWithDataFromCard(Card.fromCode("01DE001"))
-                                        .ifPresent(card -> log.info(card.toString()));
-  }
 }
